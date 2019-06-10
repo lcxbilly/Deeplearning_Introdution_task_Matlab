d8b12307de4c824e79b59eebe7f52d6645391e80
Billy <32449691+lcxbilly@users.noreply.github.com>
2019-06-10 23:01:35 +0200

    Add files via upload

---
+++ Q1_4.m
@@ -0,0 +1,86 @@
+
+digitDatasetPath = 'MNIST/Train' %fullfile(matlabroot,'toolbox','nnet','nndemos',...
+    %'nndatasets','DigitDataset');
+digitData = imageDatastore(digitDatasetPath,...
+    'IncludeSubfolders',true,'LabelSource','foldernames');
+
+testdigitDatasetPath = 'MNIST/Test' %fullfile(matlabroot,'toolbox','nnet','nndemos',...
+    %'nndatasets','DigitDataset');
+testdigitData = imageDatastore(testdigitDatasetPath,...
+    'IncludeSubfolders',true,'LabelSource','foldernames');
+
+labelCount = countEachLabel(digitData)
+
+img = readimage(digitData,1);
+size(img)
+
+% trainNumFiles = 50000;
+%[trainDigitData,valDigitData] = splitEachLabel(digitData,0.8,'randomize');
+%trainDigitData=digitData;
+%testDigitData=testdigitData;
+trainDigitData=digitData;
+testDigitData=testdigitData;
+
+%% Define Network Architecture
+
+layers = [
+    imageInputLayer(size(img))
+    
+    convolution2dLayer(3,8,'Padding',1)
+    %batchNormalizationLayer
+    maxPooling2dLayer(2,'Stride',2)
+    reluLayer
+    
+    
+    
+    convolution2dLayer(3,16,'Padding',1)
+    %batchNormalizationLayer
+    maxPooling2dLayer(2,'Stride',2)
+    reluLayer
+    
+    
+    
+    convolution2dLayer(3,32,'Padding',1)
+    %batchNormalizationLayer
+    reluLayer
+    
+    fullyConnectedLayer(10)
+    softmaxLayer
+    classificationLayer];
+
+
+options = trainingOptions('sgdm',...
+    'InitialLearnRate',0.001,...
+    'MaxEpochs',15, ...
+    'MiniBatchSize', 100, ...
+    'Shuffle','every-epoch', ...
+    'Verbose',false,...
+    'Plots','training-progress');
+
+%% Train Network Using Training Data
+% Train the network using the architecture defined by |layers|, the
+% training data, and the training options.  By default, |trainNetwork| uses
+% a GPU if one is available (requires Parallel Computing Toolbox(TM) and a
+% CUDA-enabled GPU with compute capability 3.0 or higher). Otherwise, it
+% uses a CPU. You can also specify the execution environment by using the
+% |'ExecutionEnvironment'| name-value pair argument of |trainingOptions|.
+
+%%
+% The training progress plot shows the mini-batch loss and accuracy and the
+% validation loss and accuracy. For more information on the training
+% progress plot, see
+% <docid:nnet_examples.mw_507458b6-14c3-4a31-884c-9f2119ff7e05>. The loss
+% is the <docid:nnet_ref.bu80p30-3 cross-entropy loss>. The accuracy is the
+% percentage of images that the network classifies correctly.
+net = trainNetwork(trainDigitData,layers,options);
+
+
+%predictedLabels = classify(net,valDigitData);
+%valLabels = valDigitData.Labels;
+
+%accuracy = sum(predictedLabels == valLabels)/numel(valLabels)
+
+testpredictedLabels = classify(net,testDigitData);
+testLabels = testDigitData.Labels;
+
+testaccuracy = sum(testpredictedLabels == testLabels)/numel(testLabels)
---
+++ Q2_1_lr.m
@@ -0,0 +1,95 @@
+clear;
+
+dataFolder = fullfile('WARWICK');
+imageFolderTrain = fullfile(dataFolder,'train');
+labelFolderTrain = fullfile(dataFolder,'trainlabel');
+imdsTrain = imageDatastore(imageFolderTrain);
+
+classNames = ["cell" "background"];
+labels = [255 0];
+pxdsTrain = pixelLabelDatastore(labelFolderTrain,classNames,labels);
+
+pximdsTrain = pixelLabelImageDatastore(imdsTrain,pxdsTrain);
+tbl = countEachLabel(pximdsTrain);
+
+numberPixels = sum(tbl.PixelCount);
+frequency = tbl.PixelCount / numberPixels;
+classWeights = 1 ./ frequency;
+
+inputSize = [128 128 3];
+numClasses = numel(classNames);
+%%
+layers = [
+    
+    imageInputLayer(inputSize)
+    
+    convolution2dLayer(3,64,'Padding',1)
+    batchNormalizationLayer
+    reluLayer
+    
+    convolution2dLayer(3,64,'Padding',1)
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,64,'Padding',1)
+    batchNormalizationLayer
+    reluLayer
+    
+    convolution2dLayer(3,64,'Padding',1)
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,64,'Padding',1)
+    batchNormalizationLayer
+    reluLayer
+    
+
+    transposedConv2dLayer(4,64,'Stride',2,'Cropping',1)
+    convolution2dLayer(3,64,'Padding',1)
+    batchNormalizationLayer
+    reluLayer
+    
+    
+    transposedConv2dLayer(4,64,'Stride',2,'Cropping',1)
+    convolution2dLayer(3,64,'Padding',1)
+    batchNormalizationLayer
+    reluLayer
+ 
+    
+    convolution2dLayer(1,numClasses)
+    softmaxLayer
+    pixelClassificationLayer('Classes',classNames)];
+
+
+options = trainingOptions('adam', ...
+    'MaxEpochs', 30, ...
+    'MiniBatchSize', 4, ...
+    'InitialLearnRate', 1e-3,...
+    'Shuffle','every-epoch', ...
+    'GradientDecayFactor', 0.900,...
+    'SquaredGradientDecayFactor',0.99,...
+    'Epsilon',1e-8,...
+    'LearnRateSchedule','piecewise',...
+    'LearnRateDropPeriod',3,...
+    'LearnRateDropFactor',0.9,...
+    'Plots','training-progress');
+
+net = trainNetwork(pximdsTrain,layers,options);
+%%
+imageFolderTest = fullfile(dataFolder,'test');
+imdsTest = imageDatastore(imageFolderTest);
+labelFolderTest = fullfile(dataFolder,'testlabel');
+pxdsTest = pixelLabelDatastore(labelFolderTest,classNames,labels);
+pxdsPred = semanticseg(imdsTest,net,'WriteLocation',tempdir,'MiniBatchSize',4);
+metrics = evaluateSemanticSegmentation(pxdsPred,pxdsTest);
+
+%%  dice
+dicecoff=([]);
+for i=1:60
+similarity = dice(readimage(pxdsPred,i),readimage(pxdsTest,i));
+dicecoff(i)=similarity(1);
+end
+fprintf('dice done\n')
+disp(mean(dicecoff))
